a18becae68abc51b0320b208ef00dcad3b299721
TDagger5 ⴰⵔⴰ 𐦝 <experienciaroms@gmail.com>
2025-04-27 10:22:09 -0300

    AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_manaus.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_manaus-userdebug
+    lineage_manaus-user \
+    lineage_manaus-userdebug \
+    lineage_manaus-eng
